a6afffe6fa16dc31983f3a033e3b216b3f489bb1
Bunny <144216222+B3nny444@users.noreply.github.com>
2024-08-02 03:51:30 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -9,7 +9,7 @@ class Config(object):
   TOKEN = "7008178074:AAHRxTvWF_HBlQ-FXzg5gnYhXzHpmLWEj5A"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     SESSION ="BQArz2uKal5QtRmxtlSRokb2iGQ1g8LFKGhYe-sh_Eqb8Z_lCxyT1YMc5Rj8wftbp3kQxauyTM4D46KwMXsdRBelE8Gq5g5Dk0b799JlIOBw1YhR16aYUS_8i_vrBhrx0skdiUZoE13GZQ7SvjMPfPieen9tiEkgyJLyIsTRZF3cTDXTT64wvQ4dqs8UVOqkaG_-1sZRKB8nK4-OQuBkDWJ0ATsuzxNXElcuZ00aoYi28o8AIl7lJL00E3wlpmY9RU2nWp3mXrbfesre8C-rDPwWZHlnQIyJylmLj7cYHAE3y83rJ6Q6jYvAXnuWDBEAdn6zzD1Vq6kJtXKaOVKRdlRFcwgosAA"
     OWNER_ID = 1513565142 # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    SUPPORT_CHAT = "LovelyXSupport"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "pokemonXgrabbers"  # Your own group for support, do not add the @
     START_IMG = "https://te.legra.ph/file/59d555afd67ff0d9bd224.mp4"
     PM_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
